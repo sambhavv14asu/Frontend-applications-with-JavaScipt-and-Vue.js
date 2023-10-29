87c0f9f297de5c63a92017d23ec8f3cad3165bdd
Sambhav Kapoor <112653810+sambhavv14asu@users.noreply.github.com>
2023-10-29 01:00:09 -0700

    Add files via upload

---
+++ Customizable_jeopardy.vue
@@ -0,0 +1,249 @@
+<script>
+//Playground link is : https://play.vuejs.org/#eNq9Wgtz28YR/itnVjFJkeJLVtoyJFvHcVtnaietPNPpiJoYJI4kbBBAgYMojqL/3u+euANAUVYc+yEBd3t7u3u7e98uedd4mSS9m5w2xo1JtkyDhM3mEb1N4pQRn668PGR384gQ32Neqy0eCUkpy9NIvRAS5dsFTX9a/Rx6e5pm4/NueeaVx+g6TgOajV+YyYx5KRuvvDCjxRjNsiCO3sefaDQmzaaZWEoO+zeRT2/HV9eViXfeFtytiURIc7mMUz4+6OKvtSpPUxoxKfF4WAhMb6uDyziP2Hhg3mnk/x27jYkr+y6IIpq+zdau4HInZYH9uDr1clvir8b/ldOMwRh1S6JsBxGtiTyjqRqN8jA04/9TTP5NMxylO5fRkC7FDiVNPMGI+q9oGGrbHf9bWFcylustvT7SOPFSfz8cnxXG1YMje9CP80VIf1RTlwzGs/jsvDWULVtNjJbciY/9EGQemPk1U6/TNHasKHioE5Rj9/zXvSTYeWy5GSu37/fZJsiI53/MM0bYhipnJ/FKuV5GFl5GfRJHxE/jxI93UWFyyaQUOSbAuM9FYBuRKVgHWa9E+J0mC1ataCIobHfvhTRas03BjZCQMvw3/GqoDU8ouglC2sLwLLJ5kJq1SZy02tZSvlN0dmaNCAuKBwr7C4lnj5H4M2W2pJ6UpK6VO882rYEruZC906mVXXmD8hR1+CsKh6BZ6fRVRkKye7wDFAnS8gEh9Zrq3CEmjXAHHKSgdXzk9EJ6iRPZx92kjrzeT04vajzFXf4kVzm9kM5yTPDPFd1xl7Lw9eI/zWOK/LGlbBP72ZiYvZQbyQmyoWGSkSAiOHIWRGvi6euQMH4f4ieunixPqcnqyD94y6MAA5Kpl+2jJedwad2klbyCCICdvJ0XMOnEreaGsSQb9/txgvvFX/SW8bbvJcEvYuteskn+gpGtF/nTlIrtm2Vf5BDBcOUx9jGLsbWhEja1L3gQ8zU9sYUi0xFm9KiNDLkhvV3ShJuhoAHLq+GgOzzvjobd0bfd0R+7oz93zwfd89F1SVxImOCBPtYQGmhwWxzVXXAuG4B7aAoLxts3/i2XtBBJ2MbBOJiP6I68TFNvXzaig3kOEPLNgunAvEt3D2QaqGYLOwCUfYSk4P7WY5veKozjtCUe5USrfSoPLw1uAk9bB5x0cFpBgUB+ZhTvBdEyzH2cpxxqk+fPybOasywI6ze6kuuve4HfduO32EsErdrHidyqwSXt8a0OsxHH8Sg2EShdRsE06AwfyCMyXcj14V5fI0gSI7Lk6Yms4lTBJgOpHJ8x4OuhE21dnB7yj5ILGtj2m9lJx3SZTqeuzPbxfuHtSze7yTsafbf8YLUKlgDP+64+aPPAT7zrCSTatUFv6Q4XlQ6/mNKc2tdy9Za5srlcP5sObM1l2VWWXDuhXdEYVGDKmcq2lj9MHcBOfv21YmOXon1HiBPcFXh1VRXpbHg9kZaqAQoCd08d87hzwOTQad74b5wDX0WAVCYIyMm8QTpE8sYD3jJkDoqaBj9uvDAHksjIOqU4tBQ8sXwf5ynAWeiBbt6ww44Dj6p8NfXI1I7Wh9SoRnK/z2tfRsmQMBG7kImXEJCSxzLZeECOFPejvuaBIRc5wF7MTGnmnP0DLkQsOZ9074mrXxp3OnyuHX/a7OjHTvN5ESIYL14ww/YJnS7iOKRe1OwQDPC7HlRVNNB+wo1qu5kOWI0qsAZCZFeD6562Y+06WTXXrIInpzDkL9K6pbXCHsWjlbtt3rrqB3c7ZdTLIR0YKouHMmxSZUM5i4jZovjnYAAlvjvt9gBqSZwE4EZ7CZjxiApgKio3bEnr6Jg2rj067NpeiFD099LFy/5cnxUrMl5Pp6NHp0YZlqL8d7WuC+sCMZm1ok0wnTesTAExpYTIjSrXOu5UubCOZ0fpTcfpOlWnsbKNHy/zLSZ43fg6pPzx+/0bXxrVtiBQ9yUASrRutdtAWuhc/eP923/i7D9MsgQpMmP7kE6byzhEi4RnTxo1Z+Tnk7uqUPeTPl8y+2DJUe9488YrGVTPLFu6OfcLW+vsAWv9TraC1362pYqLWiUVe9U3dS0gBy0esvabSCUxx96HMktNhBwLQzIdVZCFSIiIi8PoqxwcqqGqb86KoKKW8W+t2OQjW48XSVU/GBRlFUAxKnxeCA2/U6VPXRcp6HRsmWoRTXA9w4YuduEyBQ4MAEmNZwaFRHV4QCYa3T3m56ZQ3LzRaWGPzrDdAarhuXPHmwHoNq1hLnmmmombpXWS/g/Sl1U3a82sDupsOuQFWHl48tgAqxxm+Torcy67bU0SxqriqO0kLjlYfnAII268G2puMYUQa6QTaBFtFb7+YEYq3wRiE8WAD/ok28R5CIxG8Y/t+LV2MsTN5mx83JKVkqRoHS0oPJm+5VvW9AfdVs9D3UPDcR6BO5KR/sRnwugWEjKKZ0ImobegIY8e3HqqmT1vzN6VG9zjSV9QzoT3TRQe58WtvY7cnG1jn4YYK+UwzBG5lv+ZxKIBAHJ7Y94Quxp1z7svuhfdb6+xZOyrjj5oVGUlQRFEvLuTq+6RaSU7JVtfCifVW6RVNYsq3dHU+uTqIWXt1TX6FlzA/KDCGiJ+rsp63cNKM85APSr9mS/3Fh7dyrhvdgPeC2lzEWyPxT4qJ93dCYrO8P6ejMnJ3Z1YxrdmvuTaN+z17421jY2D+S5O88RRh48IvhuX70EFnKq80KO0AxkH3IwfTqQi9x8w9NdlGCw/YdQu/JsUzYBmt9opuhILr90ZwV3PDAcDJQA0OsFb1TqFFl9EDWLqMdmFkjfb76LcSCtXu6XQGCRfQWMUGacu0XG1t9QP8u3TFD83io9OD6oOoq+h+vnXVf2FUf38sOog+hqqv/h81TeAFk9T/MIo/uKw4iCqSX99K+MmUDxYCb0V2HUyuAF/PN8lcokf3OhFqCw4UhtwoCYeJwDX1buFT1vcBZxvqv2L9O0WJPcKJWXFjG5aYI6fFVK8A5pE3jdSWorVVdNTIbMswAW8h7p2zTRvrOKIne1osN6w8SIOfRD8INvYYpXpgeH6FaUT5NRor95YBwQRKNcVRClwXBLdCVXfMZgEUYJ2nOht6TveufYtrMr1Re+Ocdwu6eWb7ao2WAf9Zb7YBmzSl4SzwtTcUTQItXTvQ/nHWmFg1OYXrds4s1g6CqaeH8RcP95Kxbu0HmoMqS82s/pPN2c4qiV6rGtOWuoVNd9jaZOHjAO91HHwSY2xjkshvNuy+uOF+JuIi4oUiuFMTDtyFAY23t4sFdvPZFONNGFWd6r2oAo+MrJlmMjQPhTZVqFuYtsEddE/QHiioJHEcmcO9C14PxH+jgeRnvSH0Ys49VGFoIURekmG7zbpJ1WV7AKfbcbkTxffYAC1g1iMvNklSOoOjzEZJrcki2F08ofBQNdqDBKe4STW+LbREl4nOqpgJMoQIVGj22AAk9EqWIs+L76SJhgDM8bbBB8SpT8JcItLwXyGPm94YRjvfhRj3I9U/Ys1G7r8VDP+MbvlY6ixAWtpeoMjN3NA1rg25PTry3eQ2JqEm+UhqB+YxInHYS6+3SHIvs8jH2JbdELaN1v+9Tq0k95nr28ZPtLXSolAMEUfPDenrx5QvRD3vHcu1sGijfv/A5e/LRk=
+export default{
+  data(){
+    return{
+      numberOfPlayers:3,
+      numberOfCategories:4,
+      start:false,
+      sessionToken: '',
+      categoryIndex:[],
+      categoryNames:[],
+      playerScores:[0,0,0],
+      currentPlayer:1,
+      nextPlayer:1,
+      count:0,
+      endGame: false,
+      winnerMsg: '',
+      currentCategory:'',
+      currentAmount:0,
+      currentQuestion:'',
+      currentAnswer:'',
+      userAnswer:null,
+      questionResult:null,
+      selection: false,
+      answeredCells:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      selectedCell:0,
+      jeopardy1:-1,
+      jeopardy2:-1,
+      doubleJeopardyState:0,
+      wagerAmount:0,
+      wager:false,
+      wageDisabled:false,
+      wageError:'',
+      wagerMsg: ''
+    }
+  },
+   watch:{
+    //this adjust the number of players based on dropdown selection
+    numberOfPlayers(){
+      const n = this.numberOfPlayers;
+      if(n<this.playerScores.length){
+        let len = this.playerScores.length;
+        while(len>n){
+          this.playerScores.pop();
+          len--;
+        }
+      }else if(n>this.playerScores.length){
+          let len = this.playerScores.length;
+          while(len<n){
+            this.playerScores.push(0);
+            len++;
+        }
+      }
+    },
+    //this fetches the number of categories based on dropdown selection
+    numberOfCategories(){
+      this.getCategories();
+      const n = this.numberOfCategories;
+      if(n*5<this.answeredCells.length){
+        let len = this.answeredCells.length;
+        while(len>n*5){
+          this.answeredCells.pop();
+          len--;
+        }
+      }else if(n*5>this.answeredCells.length){
+          let len = this.answeredCells.length;
+          while(len<n*5){
+            this.answeredCells.push(0);
+            len++;
+        }
+      }
+    }
+  },
+  methods: {
+      //this method helps in getting a session token to ensure questions are unique
+    async getSessionToken(){
+      const res = await fetch('https://opentdb.com/api_token.php?command=request');
+      const data = await res.json();
+      this.sessionToken = data.token;
+    },
+    async getCategories(){
+      const exceptionCategories = [10,13,21,26,27,29,30,32];
+      const response = await fetch('https://opentdb.com/api_category.php');
+      const data = await response.json();
+      let randomIdx = [];
+      this.categoryIndex = new Array();
+      this.categoryNames = new Array();
+      let i=0;
+      while(i<this.numberOfCategories){
+        const random = Math.floor(Math.random()*data.trivia_categories.length);
+        if(!randomIdx.includes(random) && !exceptionCategories.includes(data.trivia_categories[random].id)){
+          randomIdx.push(random);
+          this.categoryIndex.push(data.trivia_categories[random].id);
+          this.categoryNames.push(data.trivia_categories[random].name);
+          i=i+1;
+        }
+      }
+        //randomly selecting 2 cells for double jeopardy
+      this.jeopardy1 = Math.floor(Math.random()*(5*this.numberOfCategories));
+      this.jeopardy2 = Math.floor(Math.random()*(5*this.numberOfCategories));
+      while(this.jeopardy2==this.jeopardy1){
+        this.jeopardy2 = Math.floor(Math.random()*(5*this.numberOfCategories));
+      }
+    },
+    async getQuestion(difficulty,category,categoryName,amount,selectedCell){
+      this.start = true;
+      if(this.answeredCells[selectedCell]!=0){
+        return;
+      }
+      this.currentPlayer = this.nextPlayer;
+      if(this.jeopardy1 == selectedCell || this.jeopardy2 == selectedCell){  
+        if(this.playerScores[this.currentPlayer-1]<amount){
+          this.wager=true;
+          this.wagerMsg = "You can only select $" + amount + " since face value is greater than your balance";
+        }else{
+          this.doubleJeopardyState=1;
+          this.wager=true;
+        }
+      }
+      // state 1 tells that this cell has been questioned but not answered
+      this.answeredCells[selectedCell] =1;
+      const response = await fetch('https://opentdb.com/api.php?amount=1&category='+category+'&difficulty='+difficulty+'&type=boolean'+ '&token='+ this.sessionToken)
+      const data = await response.json();
+      this.currentQuestion = data.results[0].question;
+      this.currentAnswer = data.results[0].correct_answer;
+      this.count=this.count+1;
+      this.currentCategory = categoryName;
+      this.currentAmount = amount;
+      this.selection = true;
+      this.userAnswer = null;
+      this.questionResult = null;
+      this.selectedCell = selectedCell;
+    },
+     validateAnswer(answer){
+    // state 2 tells that this cell has already been answered
+      if(this.answeredCells[this.selectedCell]==2){
+        return;
+      }
+      this.wager=false;
+      this.doubleJeopardyState=0;
+      this.wageError="";
+      if(answer === this.currentAnswer){
+        this.playerScores[this.currentPlayer-1]=this.playerScores[this.currentPlayer-1]+this.currentAmount;
+        document.getElementById(this.selectedCell.toString()).innerHTML = `<span style='color: green'> P${this.currentPlayer}</span>`;
+        this.questionResult = "Correct!";
+      }else{
+      this.playerScores[this.currentPlayer-1]=this.playerScores[this.currentPlayer-1]-this.currentAmount;
+      document.getElementById(this.selectedCell.toString()).innerHTML = `<span style='color: red'> P${this.currentPlayer}</span>`;
+        this.nextPlayer=this.currentPlayer%this.numberOfPlayers+1;
+        this.questionResult = "Incorrect!";
+      }
+      this.selection =false;
+      this.answeredCells[this.selectedCell] =2;
+      if(this.count ==5*this.numberOfCategories){
+        this.endGame=true;
+
+      }
+      let idx=0;
+      let max = this.playerScores[0];
+      for(let i=1;i<this.playerScores.length;i++){
+        if(this.playerScores[i]>max){
+          idx=i;
+          max=this.playerScores[i];
+        }
+      }
+      this.winnerMsg= "Player "+(idx+1)+" has won the game!";
+
+      
+    },
+    validateWage(){
+      if(this.wagerAmount>=1 && this.wagerAmount<=this.playerScores[this.currentPlayer-1]){
+        this.currentAmount = this.wagerAmount;
+        this.doubleJeopardyState = 0;
+        this.wagerAmount=0;
+        this.wagerMsg = "You have selected $" + this.currentAmount + " to wage";
+      }else{
+        this.wageError = "Amount waged should be between $1 and $" + this.playerScores[this.currentPlayer-1];
+      }
+    }
+  },
+  beforeMount(){
+      this.getSessionToken()
+      this.getCategories()
+  },
+ 
+}
+</script>
+<template>
+  <label for="players">Number of players:</label> 
+  <select name="players" v-model="numberOfPlayers"  > 
+      <option v-for="player in [2,3,4,5,6]" :disabled="start == true">{{player}}</option> 
+  </select>
+  <br>
+  <label for="categories">Number of Categories:</label> 
+  <select name="categories" v-model="numberOfCategories"> 
+      <option v-for="category in [2,3,4,5,6]" :disabled="start == true">{{category}}</option> 
+  </select>
+  <table>
+  <tr>
+  <td v-for = "(score,index) in playerScores">Player {{index+1}} : ${{score}}</td>
+  </tr>
+  <tr>
+  <th v-for = "categoryName in categoryNames">{{categoryName}}</th>
+  </tr>
+    <tr>
+  <td v-for = "(categoryName,index) in categoryNames" :id="`${index}`" @click="getQuestion('easy',this.categoryIndex[index],this.categoryNames[index],100,index)">$100</td>
+  </tr>
+    <tr>
+   <td v-for = "(categoryName,index) in categoryNames" :id="`${index +categoryNames.length}`" @click="getQuestion('easy',this.categoryIndex[index],this.categoryNames[index],200,index+categoryNames.length)">$200</td>
+  </tr>
+    <tr>
+   <td v-for = "(categoryName,index) in categoryNames" :id="`${index + 2 * categoryNames.length}`" @click="getQuestion('medium',this.categoryIndex[index],this.categoryNames[index],300,index+2*categoryNames.length)">$300</td>
+  </tr>
+    <tr>
+   <td v-for = "(categoryName,index) in categoryNames" :id="`${index + 3 * categoryNames.length}`" @click="getQuestion('medium',this.categoryIndex[index],this.categoryNames[index],400,index+3*categoryNames.length)">$400</td>
+  </tr>
+    <tr>
+   <td v-for = "(categoryName,index) in categoryNames" :id="`${index + 4 * categoryNames.length}`" @click="getQuestion('hard',this.categoryIndex[index],this.categoryNames[index],500,index+4*categoryNames.length)">$500</td>
+  </tr>
+  </table>
+  <p v-if = "endGame == true">{{winnerMsg}}</p>
+  <div v-if = 'count>0 && count<=5*numberOfCategories && endGame == false'>
+  <p>Player {{currentPlayer}} selects {{currentCategory}} for ${{currentAmount}} : </p>
+  <p v-if = "doubleJeopardyState==0 && wager==true"><span style="font-weight:bold">Double wager question:</span> {{wagerMsg}}</p>
+  <div v-if = "doubleJeopardyState==1 && wager==true">
+  <p style="font-weight:bold">Double jeopardy wager:<input type="number" v-model="wagerAmount"><button type="button" @click="validateWage">Submit</button></p>
+  <p>{{wageError}}</p>
+  </div>
+  <div v-if = "doubleJeopardyState==0">
+  <p>{{currentQuestion}}</p>
+  <input type="radio" value="true"  v-model = userAnswer v-on:change="validateAnswer('True')"><label for="true">True</label>
+  <input type="radio" value="false" v-model= userAnswer v-on:change="validateAnswer('False')"><label for="false">False</label>
+  </div>
+  <p v-if = 'questionResult != null '>{{questionResult}}</p>
+  </div>
+  <p v-if = 'count>=0 && count<5*numberOfCategories && selection == false'>Player {{nextPlayer}} to select</p>
+
+</template>
+<style>
+table {
+    border-collapse: collapse;
+    width: 85%;
+}
+table,th, td {
+    border: 1px solid #000;
+    text-align: center;
+}
+
+</style>
